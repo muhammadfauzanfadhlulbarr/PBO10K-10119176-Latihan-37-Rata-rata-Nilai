9fc0d95087ea809177b327fa6d9b25314249e305
Muhammad Fauzan <muhammadfauzanfadhlulbarr@gmail.com>
2020-11-04 18:52:19 +0700

    first commit

---
+++ Latihan37.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+ * Latihan34
+ * Nama     : Muhammad Fauzan Fadhlulbarr
+ * Kelas    : PBO10K
+ * NIM      : 10119176
+ * Program  : Program Rata-rata Nilai
+ */
+
+class RatarataNilai {
+    double Ratarata;
+    double jumlahNilai = 0;
+    
+    public void hitungNilai(int jumlahMhs){
+        int i;
+        double nilai;
+        Scanner scanner = new Scanner(System.in);
+        
+        for (i = 1; i <= jumlahMhs; i++) {
+            System.out.print("Nilai Mahasiswa ke-" + i + " : ");
+            nilai = scanner.nextDouble();
+            jumlahNilai = jumlahNilai + nilai;
+        }
+    }
+    
+    public double hitungRatarataNilai(double jumlahNilai, int jumlahMhs) {
+        Ratarata = jumlahNilai / jumlahMhs;
+        return Ratarata;
+    }
+    
+}
---
+++ PBO10k10119176Latihan37.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Latihan34
+ * Nama     : Muhammad Fauzan Fadhlulbarr
+ * Kelas    : PBO10K
+ * NIM      : 10119176
+ * Program  : Program Rata-rata Nilai
+ */
+public class PBO10k10119176Latihan37 {
+    public static void main(String[] args) {
+        int jumlahMhs;
+        Scanner scanner = new Scanner(System.in);
+        RatarataNilai Ratarata = new RatarataNilai();
+        
+        System.out.print("Masukan Banyaknya Mahasiswa : ");
+        jumlahMhs = scanner.nextInt();
+        
+        Ratarata.hitungNilai(jumlahMhs);
+        double jumlahNilai;
+        System.out.println("\n" + "Rata Rata Nilainya adalah : " + 
+        Ratarata.hitungRatarataNilai(Ratarata.jumlahNilai,jumlahMhs));
+    } 
+}
